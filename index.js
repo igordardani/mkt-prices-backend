173cf0647de04b754de4b2c8fdb9c56dc98d0c89
igordardani <igor.dardani@gmail.com>
2026-06-16 23:46:32 -0300

    feat: importação de NF-e via PDF usando Gemini

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer')
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: "20mb" }))
 
 app.get('/', (req, res) => {
   res.json({ status: 'mkt-prices backend rodando!' })
